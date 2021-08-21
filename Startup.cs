a64921138fa2ff1c3f96a90cf0d3a67e911d7f49
manassesj <jmanasses1024@hotmail.com>
2021-08-21 11:02:30 -0300

    xml swagger configuration

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using APIcatalogo.Controllers.v1;
 using APIcatalogo.Repositories;
 using APIcatalogo.Services;
 using Microsoft.AspNetCore.Builder;
@@ -28,13 +31,20 @@ namespace APIcatalogo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IExemploSingleton, ExemploCicloDeVida>();
+            services.AddScoped<IExemploScoped, ExemploCicloDeVida>();
+            services.AddTransient<IExemploTransient, ExemploCicloDeVida>();
             services.AddScoped<IJogoService, JogoService>();
             services.AddScoped<IJogoRepository, JogoRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIcatalogo", Version = "v1" });
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
