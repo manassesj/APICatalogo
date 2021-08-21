e95511588c4bc169b6dae30eca5d35cf4e0d1a89
manassesj <jmanasses1024@hotmail.com>
2021-08-20 23:05:31 -0300

    Model and Controller

---
+++ Services/IJogoService.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using APIcatalogo.InputModel;
+using APIcatalogo.ViewModel;
+
+namespace APIcatalogo.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+
+    }
+}
---
+++ ViewModel/JogoViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace APIcatalogo.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+        
+    }
+}
